f498452292d8ae987764c70b4cbcc1c28439a565
baliram123giri <gir71401@gmail.com>
2022-03-25 11:46:03 +0530

    users-list

---
+++ src/components/pages/Users.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Users() {
+  return (
+    <>
+      <div className="container my-3 my-md-5">
+        <h6>GitHub Users List</h6>
+      </div>
+    </>
+  )
+}
